a056d46dc1ae6d136e92141611fe04322fab3c73
Nikita Balabaev <xakep139@users.noreply.github.com>
2023-06-06 16:22:30 +0200

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,3 +1,6 @@
+# This file was taken from https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
+# And modified to fetch the Preview version of winget
+
 $progressPreference = 'silentlyContinue'
 $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/103905297).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
